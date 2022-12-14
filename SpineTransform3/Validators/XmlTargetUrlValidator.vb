436b0259eb01d1d994af6cdfc4a71f8f5a54a40c
Jon Gorman <jtgorman@illinois.edu>
2022-12-13 19:15:49 -0600

    Added first set of testing cases around validating target urls, going work on implementing the target url validation

---
+++ SpineTransform3/Validators/XmlTargetUrlValidator.vb
@@ -0,0 +1,20 @@
+﻿Public Class XmlTargetUrlValidator
+
+    Public Shared Function Validate(raw_url As String) As Boolean
+        Throw New NotImplementedException
+        '    Dim url As Uri = New Uri(raw_url)
+
+        '    Dim uiucLibraryHostname As String = ".library.illinois.edu"
+
+        '    Dim hostname As String = url.Host
+
+        '    Dim is_valid As Boolean = hostname.ToLower().Equals("library.illinois.edu") _
+        '                             OrElse (hostname.EndsWith(uiucLibraryHostname) _
+        '                                    AndAlso hostname.EndsWith(".xml"))
+
+        '  Return is_valid
+
+    End Function
+
+
+End Class
